7e43a68c27340a0b293f83ddc172e5925acefedf
6777 <iverson.lee@tradevan.com.tw>
2024-09-02 14:18:33 +0800

    Get invoices

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import SideNav from "@/app/ui/dashboard/sidenav";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    console.log(children);
+//   console.log("-----Layout-----");
+//   console.log(children);
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
